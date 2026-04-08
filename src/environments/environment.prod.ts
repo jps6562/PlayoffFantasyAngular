70cce6d30107367ac6f2a341d39373abd6532daa
jselnekovicjr <jselnekovicjr@gmail.com>
2026-04-08 15:26:36 -0400

    Angular 18 upgrade and Auth0 configuration fixed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,11 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'playofffantasy-api-hzawdkg6c7acgcg9.centralus-01.azurewebsites.net/api'
+  apiBaseUrl: 'playofffantasy-api-hzawdkg6c7acgcg9.centralus-01.azurewebsites.net/api',
+  auth0: {
+    domain: 'dev-x7ylktswpnvh1233.us.auth0.com',
+    clientId: 'z3Bm23FJuaf5B7vGBH6qBBVJPZv9zC4P',
+    authorizationParams: {
+      redirect_uri: typeof window !== 'undefined' ? window.location.origin : ''
+    }
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,7 +7,10 @@ export const environment = {
   apiBaseUrl: 'http://localhost:7228/api',
   auth0: {
     domain: 'dev-x7ylktswpnvh1233.us.auth0.com',
-    clientId: 'z3Bm23FJuaf5B7vGBH6qBBVJPZv9zC4P'
+    clientId: 'z3Bm23FJuaf5B7vGBH6qBBVJPZv9zC4P',
+    authorizationParams: {
+      redirect_uri: typeof window !== 'undefined' ? window.location.origin : ''
+    }
   }
 };
 
